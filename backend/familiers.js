af30f8cee904b23a862f02c5a35651994a104352
Lacroix-Eva <eva.lacroix@edu.univ-fcomte.fr>
2026-01-29 19:05:03 +0100

    Choisir son familier

---
+++ backend/familiers.js
@@ -0,0 +1,23 @@
+export const FAMILIERS = {
+
+  plume: {
+    name: "Plume",
+    svg: "/familiers/plume.png",
+    alt: "Plume le poussin",
+    description: "Calme et apaisant",
+  },
+
+  sunny: {
+    name: "Sunny",
+    svg: "/familiers/sunny.png",
+    alt: "Sunny la girafe",
+    description: "Observatrice et attentive",
+  },
+
+    buzzy: {
+    name: "Buzzy",
+    svg: "/familiers/buzzy.png",
+    alt: "Buzzy l’abeille",
+    description: "Énergique et motivante",
+  },
+};
---
+++ pocketbase/pb_migrations/1769697232_updated_users.js
@@ -0,0 +1,80 @@
+/// <reference path="../pb_data/types.d.ts" />
+migrate((app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "oauth2": {
+      "mappedFields": {
+        "name": ""
+      }
+    }
+  }, collection)
+
+  // add field
+  collection.fields.addAt(8, new Field({
+    "hidden": false,
+    "id": "select3998748506",
+    "maxSelect": 1,
+    "name": "familier",
+    "presentable": false,
+    "required": false,
+    "system": false,
+    "type": "select",
+    "values": [
+      "Plume",
+      "Sunny",
+      "Buzzy"
+    ]
+  }))
+
+  // update field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1579384326",
+    "max": 255,
+    "min": 0,
+    "name": "pseudo",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+}, (app) => {
+  const collection = app.findCollectionByNameOrId("_pb_users_auth_")
+
+  // update collection data
+  unmarshal({
+    "oauth2": {
+      "mappedFields": {
+        "name": "name"
+      }
+    }
+  }, collection)
+
+  // remove field
+  collection.fields.removeById("select3998748506")
+
+  // update field
+  collection.fields.addAt(6, new Field({
+    "autogeneratePattern": "",
+    "hidden": false,
+    "id": "text1579384326",
+    "max": 255,
+    "min": 0,
+    "name": "name",
+    "pattern": "",
+    "presentable": false,
+    "primaryKey": false,
+    "required": false,
+    "system": false,
+    "type": "text"
+  }))
+
+  return app.save(collection)
+})
