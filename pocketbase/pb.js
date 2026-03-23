c19dae842772942700ddf97f236ddaac054f19a3
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2026-03-23 09:15:27 +0100

    test changement de place

--- pocketbase/pb.js
+++
@@ -1,4 +0,0 @@
-import PocketBase from 'pocketbase';
-
-const pb = new PocketBase('https://moodpocketbase.victor-landwerlin.fr');
-export default pb;
---
+++ public/pocketbase/pb.js
@@ -0,0 +1,5 @@
+import PocketBase from 'https://cdn.jsdelivr.net/npm/pocketbase@0.26.7/dist/pocketbase.es.mjs';
+
+const pb = new PocketBase('https://moodpocketbase.victor-landwerlin.fr');
+
+export default pb;
