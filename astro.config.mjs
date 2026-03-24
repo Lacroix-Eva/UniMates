bceb247fa33565aa69f05ee33898b5c6c0acdd7d
Victor-KiViK-Landwerlin <150051582+Victor-KiViK-Landwerlin@users.noreply.github.com>
2026-03-24 23:54:03 +0100

    c'est bon frr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import node from '@astrojs/node';
-
 import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
@@ -10,9 +8,6 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
   },
-  output: 'server',
-  adapter: node({
-    mode: 'standalone'
-  }),
+  output: 'static',
   trailingSlash: 'ignore',
 });
